b07ddd0e02f79ab507131bd106c9d35abc8d37ea
Tony Wilson <thinqa@myopera.com>
2013-12-09 15:06:33 +0200

    Start...

---
+++ opengl_test.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2013 Tony Wilson. All rights reserved.
+// See LICENCE file for permissions and restrictions.
+
+package opengl
+
+import "testing"
+
+func TestInit(t *testing.T) {}
